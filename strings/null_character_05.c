ce0ed554e22da606d3cc243cd90e03729f842395
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-07-19 16:21:32 +0300

    Create null_character_05.c

---
+++ strings/null_character_05.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define			SIZE		40
+
+int main(void)
+{
+	char name[SIZE];
+
+	name[0] = 'A';
+	name[1] = 'H';
+	name[2] = 'M';
+	name[3] = 'E';
+	name[4] = 'T';
+
+	//yazinin sonunda null karakter yok
+
+	for (int i = 0; name[i] != '\0'; ++i)  //tanimsiz davranis
+		putchar(name[i]);
+
+	putchar('\n');
+}
