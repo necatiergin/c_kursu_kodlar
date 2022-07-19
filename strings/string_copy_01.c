fe835fa09b10d4e1947ac1a02b3256fdbba21c98
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-07-19 16:24:31 +0300

    Create string_copy_01.c

---
+++ strings/string_copy_01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	char s1[100] = "nurullah";
+	char s2[100];
+
+	//s2 = s1;  //gecersiz
+
+	int i;
+
+	for (i = 0; s1[i] != '\0'; ++i)
+		s2[i] = s1[i];
+
+	s2[i] = '\0';
+
+	puts(s1);
+	puts(s2);
+
+}
